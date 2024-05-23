69f9441aec2e8678e160215e49b643c482883372
kakelay <kakelay18052002@gmail.com>
2024-05-23 17:49:03 +0700

    update with wechat_assets_picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,268 +1,111 @@
-import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:mime/mime.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Image Picker Demo',
-      home: MyHomePage(title: 'Image Picker Example'),
+    return MaterialApp(
+      title: 'Asset Picker Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, this.title});
-
-  final String? title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<XFile>? _mediaFileList;
+  List<AssetEntity> _selectedAssets = [];
 
-  void _setImageFileListFromFile(XFile? value) {
-    _mediaFileList = value == null ? null : <XFile>[value];
-  }
-
-  dynamic _pickImageError;
-
-  final ImagePicker _picker = ImagePicker();
-  final TextEditingController maxWidthController = TextEditingController();
-  final TextEditingController maxHeightController = TextEditingController();
-  final TextEditingController qualityController = TextEditingController();
-
-  Future<void> _onImageButtonPressed(
-    ImageSource source, {
-    required BuildContext context,
-    bool isMultiImage = false,
-    bool isMedia = false,
-  }) async {
-    if (context.mounted) {
-      if (isMultiImage) {
-        await _displayPickImageDialog(context,
-            (double? maxWidth, double? maxHeight, int? quality) async {
-          try {
-            final List<XFile> pickedFileList = isMedia
-                ? await _picker.pickMultipleMedia(
-                    maxWidth: maxWidth,
-                    maxHeight: maxHeight,
-                    imageQuality: quality,
-                  )
-                : await _picker.pickMultiImage(
-                    maxWidth: maxWidth,
-                    maxHeight: maxHeight,
-                    imageQuality: quality,
-                  );
-            setState(() {
-              _mediaFileList = pickedFileList;
-            });
-          } catch (e) {
-            setState(() {
-              _pickImageError = e;
-            });
-          }
-        });
-      } else {
-        await _displayPickImageDialog(context,
-            (double? maxWidth, double? maxHeight, int? quality) async {
-          try {
-            final XFile? pickedFile = await _picker.pickImage(
-              source: source,
-              maxWidth: maxWidth,
-              maxHeight: maxHeight,
-              imageQuality: quality,
-            );
-            setState(() {
-              _setImageFileListFromFile(pickedFile);
-            });
-          } catch (e) {
-            setState(() {
-              _pickImageError = e;
-            });
-          }
-        });
-      }
-    }
-  }
-
-  Widget _previewImages() {
-    if (_mediaFileList != null) {
-      return Semantics(
-        label: 'image_picker_example_picked_images',
-        child: ListView.builder(
-          key: UniqueKey(),
-          itemBuilder: (BuildContext context, int index) {
-            final String? mime = lookupMimeType(_mediaFileList![index].path);
-
-            return Semantics(
-                label: 'image_picker_example_picked_image',
-                child: Image.file(
-                  File(_mediaFileList![index].path),
-                  errorBuilder: (BuildContext context, Object error,
-                      StackTrace? stackTrace) {
-                    return const Center(
-                        child: Text('This image type is not supported'));
-                  },
-                ));
-          },
-          itemCount: _mediaFileList!.length,
-        ),
-      );
-    } else if (_pickImageError != null) {
-      return Text(
-        'Pick image error: $_pickImageError',
-        textAlign: TextAlign.center,
-      );
-    } else {
-      return Icon(Icons.person,size: 50,);
-    }
-  }
-
-  Widget _handlePreview() {
-    return _previewImages();
-  }
-
-  Future<void> retrieveLostData() async {
-    final LostDataResponse response = await _picker.retrieveLostData();
-    if (response.isEmpty) {
-      return;
-    }
-    if (response.file != null) {
-      setState(() {
-        if (response.files == null) {
-          _setImageFileListFromFile(response.file);
-        } else {
-          _mediaFileList = response.files;
-        }
-      });
-    }
+  void _onSelectAssets(List<AssetEntity> assets) {
+    setState(() {
+      _selectedAssets = List<AssetEntity>.from(assets);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title!),
+        title: Text('Asset Picker Example'),
       ),
       body: Center(
-        child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
-            ? FutureBuilder<void>(
-                future: retrieveLostData(),
-                builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                    case ConnectionState.waiting:
-                      return const Text(
-                        'You have not yet picked an image.',
-                        textAlign: TextAlign.center,
-                      );
-                    case ConnectionState.done:
-                      return _handlePreview();
-                    case ConnectionState.active:
-                      return const Text(
-                        'You have not yet picked an image.',
-                        textAlign: TextAlign.center,
-                      );
-                  }
-                },
-              )
-            : _handlePreview(),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          IconButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Choose an Action'),
-                    content: const Text(
-                        'Do you want to open an image or take a picture?'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                          _onImageButtonPressed(
-                            ImageSource.gallery,
-                            context: context,
-                            isMultiImage: true,
-                          );
-                          print('Open Image');
-                        },
-                        child: const Text('Open Image'),
-                      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                AssetPicker.pickAssets(context,
+                    pickerConfig: AssetPickerConfig(
+                      specialItemPosition: SpecialItemPosition.prepend,
+                      specialItemBuilder: (context, path, length) {
+                        return GestureDetector(
+                          onTap: () async {
+                            final img = await ImagePicker.platform
+                                .pickImage(source: ImageSource.camera);
 
-                      ///
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                          _onImageButtonPressed(ImageSource.camera,
-                              context: context);
-                          print('Take Picture');
-                        },
-                        child: const Text('Take Picture'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            icon: const Icon(Icons.camera_alt_rounded),
-          ),
-        ],
+                            if (img != null) {
+                              Navigator.pop(context);
+                              showModalBottomSheet(
+                                  context: context,
+                                  isScrollControlled: true,
+                                  useSafeArea: true,
+                                  showDragHandle: true,
+                                  builder: (context) {
+                                    return Image.file(File(img.path));
+                                  });
+                            }
+                          },
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.red,
+                          ),
+                        );
+                      },
+                    ));
+              },
+              child: Text('Pick Images and Videos'),
+            ),
+            SizedBox(height: 20),
+            if (_selectedAssets.isNotEmpty)
+              Column(
+                children: [
+                  Text('Selected Images and Videos:'),
+                  SizedBox(height: 10),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                    ),
+                    itemCount: _selectedAssets.length,
+                    itemBuilder: (context, index) {
+                      return Image(
+                        image: AssetEntityImageProvider(_selectedAssets[index],
+                            isOriginal: false),
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
     );
   }
-
-  Future<void> _displayPickImageDialog(
-      BuildContext context, OnPickImageCallback onPick) async {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            actions: <Widget>[
-              TextButton(
-                child: const Text('CANCEL'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                  child: const Text('PICK'),
-                  onPressed: () {
-                    final double? width = maxWidthController.text.isNotEmpty
-                        ? double.parse(maxWidthController.text)
-                        : null;
-                    final double? height = maxHeightController.text.isNotEmpty
-                        ? double.parse(maxHeightController.text)
-                        : null;
-                    final int? quality = qualityController.text.isNotEmpty
-                        ? int.parse(qualityController.text)
-                        : null;
-                    onPick(width, height, quality);
-                    Navigator.of(context).pop();
-                  }),
-            ],
-          );
-        });
-  }
 }
-
-typedef OnPickImageCallback = void Function(
-    double? maxWidth, double? maxHeight, int? quality);
